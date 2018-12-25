b66db04e43dab7d352c37a93e407b0d76641127f
SamuraiWarmonger <misc.samchris@gmail.com>
2018-12-25 21:23:00 +0530

    Update 17.py

--- 17.py
+++ 17.py
@@ -3,7 +3,10 @@ n,k=input().split()
 n,k=int(n),int(k)
 ar=input()
 ar=[int(x) for x in ar.split()]
-ans=reduce(lambda x,y: x or y,[ar[x]+ar[y]==k for x in range(n) for y in range(n) if x!=y ])
+if n>1:
+    ans=reduce(lambda x,y: x or y,[ar[x]+ar[y]==k for x in range(n) for y in range(n) if x!=y ])
+else:
+    ans=ar[0]==k
 if ans:
     print("yes")
 else:
